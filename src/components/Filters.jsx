f4b6736e619083da2e2cf68426d22fa9dc5e797e
Vishal kumar <vishalkumar756380@gmail.com>
2025-02-28 18:08:32 +0530

    date range filter applied

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,33 +6,49 @@ const Filters = ({ setSelectedUsers }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [status, setStatus] = useState("all");
   const [selectedDate, setSelectedDate] = useState("");
+  const [startingDate, setStartingDate] = useState("");
+  const [endingDate, setEndingDate] = useState("");
 
   const handleClear = () => {
     setSearchTerm("");
     setStatus("all");
     setSelectedDate("");
+    setStartingDate("");
+    setEndingDate("");
     setSelectedUsers(Records);
   };
 
   const handleSearchChange = (e) => {
     const value = e.target.value;
     setSearchTerm(value);
-    filterUsers(value, status, selectedDate);
+    filterUsers(value, status, selectedDate, startingDate, endingDate);
   };
 
   const handleStatusChange = (e) => {
     const value = e.target.value;
     setStatus(value);
-    filterUsers(searchTerm, value, selectedDate);
+    filterUsers(searchTerm, value, selectedDate, startingDate, endingDate);
   };
 
   const handleDateChange = (e) => {
     const value = e.target.value; // YYYY-MM-DD from input
     setSelectedDate(value);
-    filterUsers(searchTerm, status, value);
+    filterUsers(searchTerm, status, value, startingDate, endingDate);
   };
 
-  const filterUsers = (search, statusFilter, dateFilter) => {
+  const handleDateChangeStarting = (e) => {
+    const value = e.target.value; // YYYY-MM-DD from input
+    setStartingDate(value);
+    filterUsers(searchTerm, status, selectedDate, value, endingDate);
+  };
+
+  const handleDateChangeEnding = (e) => {
+    const value = e.target.value; // YYYY-MM-DD from input
+    setEndingDate(value);
+    filterUsers(searchTerm, status, selectedDate, startingDate, value);
+  };
+
+  const filterUsers = (search, statusFilter, dateFilter, startingDate, endingDate) => {
     let filteredUsers = Records.filter((user) =>
       user.about.name.toLowerCase().includes(search.trim().toLowerCase())
     );
@@ -50,10 +66,29 @@ const Filters = ({ setSelectedUsers }) => {
       );
     }
 
+    if (startingDate) {
+      filteredUsers = filteredUsers.filter(
+        (user) => {
+          const formattedUserDate = user.details.date.split(".").reverse().join("-");
+          return formattedUserDate >= startingDate;
+        }
+      );
+    }
+
+    if (endingDate) {
+      filteredUsers = filteredUsers.filter(
+        (user) => {
+          const formattedUserDate = user.details.date.split(".").reverse().join("-");
+          return formattedUserDate <= endingDate;
+        }
+      );
+    }
+
     setSelectedUsers(filteredUsers);
   };
+
   return (
-    <div className="flex justify-between mb-8  text-lg font-medium items-center">
+    <div className="flex justify-between mb-8 text-lg font-medium items-center">
       <div className="relative border">
         <MdSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
         <input
@@ -65,7 +100,7 @@ const Filters = ({ setSelectedUsers }) => {
         />
       </div>
 
-      <div className="flex gap-8">
+      <div className="flex items-center gap-4">
         <select
           value={status}
           onChange={handleStatusChange}
@@ -76,13 +111,19 @@ const Filters = ({ setSelectedUsers }) => {
           <option value="Inactive">Inactive</option>
           <option value="Blocked">Blocked</option>
         </select>
+        Starting Date
+        <input type="date" value={startingDate} onChange={handleDateChangeStarting} placeholder="Enter Starting Date"
+        className="border p-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-black" />
+        Ending Date
+        <input type="date" value={endingDate} onChange={handleDateChangeEnding} placeholder="Enter Ending Date"
+        className="border p-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-black" />
 
-        <input
+        {/* <input
           type="date"
           value={selectedDate}
           onChange={handleDateChange}
           className="border p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-        />
+        /> */}
         <button onClick={handleClear} className="bg-white px-4 py-2 rounded-md">
           Clear Filters
         </button>
